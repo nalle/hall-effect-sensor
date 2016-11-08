f10bef900475dc37f4b77946c7000cd6650c3e95
Rickard Eriksson <rickard.eriksson@gigabit.nu>
2016-11-08 21:39:11 +0100

    Changed client-hostname to begin with PP

--- arduino/src/UIPEthernet/Dhcp.h
+++ arduino/src/UIPEthernet/Dhcp.h
@@ -44,7 +44,7 @@
 #define MAGIC_COOKIE		0x63825363
 #define MAX_DHCP_OPT	16
 
-#define HOST_NAME "ENC28J"
+#define HOST_NAME "PP"
 #define DEFAULT_LEASE	(900) //default lease time in seconds
 
 #define DHCP_CHECK_NONE         (0)
