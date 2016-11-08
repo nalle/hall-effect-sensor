b61f4e07cb4d9e5a0c3305ea4b71f2c228fc1995
Rickard Eriksson <Rickard Eriksson>
2016-11-08 21:18:46 +0100

    Ran autoformat on code due to different tabspaces being used

--- arduino/src/hall-effect-sensor/hall-effect-sensor.ino
+++ arduino/src/hall-effect-sensor/hall-effect-sensor.ino
@@ -14,9 +14,9 @@
 #define calibrationValue 6.666667
 
 typedef struct {
-	EnergyMonitor one;
-	EnergyMonitor two;
-	EnergyMonitor three;
+  EnergyMonitor one;
+  EnergyMonitor two;
+  EnergyMonitor three;
 } Sensors;
 
 Sensors sensors;
@@ -25,81 +25,81 @@ static byte mac[] = { 0x02, 0x00, 0x00, 0x00, 0x00, 0x00 };
 EthernetServer server = EthernetServer(1337);
 
 void setup () {
-	Serial.begin(57600);
-	Serial.print("Booting up...");
+  Serial.begin(57600);
+  Serial.print("Booting up...");
 
-	if (EEPROM.read(1) == '#') {
-		for (int i = 4; i < 6; i++) {
-	    	mac[i] = EEPROM.read(i);
-	    }
-	} else {	
-		randomSeed(analogRead(samplePin));
-		for (int i = 4; i < 6; i++) {
-			mac[i] = random(0, 255);
-			EEPROM.write(i, mac[i]);
-		}
-		EEPROM.write(1, '#');
-	}
-	Ethernet.begin(mac, (DhcpOptionParser *) &dhcpOptionParser, (DhcpOptionProvider *) &dhcpOptionProvider);
-	server.begin();
+  if (EEPROM.read(1) == '#') {
+    for (int i = 4; i < 6; i++) {
+      mac[i] = EEPROM.read(i);
+    }
+  } else {
+    randomSeed(analogRead(samplePin));
+    for (int i = 4; i < 6; i++) {
+      mac[i] = random(0, 255);
+      EEPROM.write(i, mac[i]);
+    }
+    EEPROM.write(1, '#');
+  }
+  Ethernet.begin(mac, (DhcpOptionParser *) &dhcpOptionParser, (DhcpOptionProvider *) &dhcpOptionProvider);
+  server.begin();
 
-	configureSensors();
-	Serial.println(" done");
-	return;
+  configureSensors();
+  Serial.println(" done");
+  return;
 }
 
 void dhcpOptionParser(const uint8_t optionType, EthernetUDP *dhcpUdpSocket) {
-    uint8_t opt_len = dhcpUdpSocket->read();
+  uint8_t opt_len = dhcpUdpSocket->read();
 
-    if (optionType == 15) { // domain name
-        // read the value with dhcpUdpSocket->read()
-    } else {
-        while (opt_len--) {
-            dhcpUdpSocket->read();
-        }        
+  if (optionType == 15) { // domain name
+    // read the value with dhcpUdpSocket->read()
+  } else {
+    while (opt_len--) {
+      dhcpUdpSocket->read();
     }
+  }
 }
 
 void dhcpOptionProvider(const uint8_t messageType, EthernetUDP *dhcpUdpSocket) {
-    char m[5];
-    snprintf(m, 5, "%02x%02x", mac[4], mac[5]);
-    uint8_t buffer[] = {
-        dhcpClientIdentifier,
-        0x06,
-        'p','p',m[0],m[1],m[2],m[3],
-    };
-    dhcpUdpSocket->write(buffer, 8);
+  char m[5];
+  snprintf(m, 5, "%02x%02x", mac[4], mac[5]);
+  uint8_t buffer[] = {
+    dhcpClientIdentifier,
+    0x06,
+    'p', 'p', m[0], m[1], m[2], m[3],
+  };
+  dhcpUdpSocket->write(buffer, 8);
 }
 
 void configureSensors() {
-	sensors.one.current(3, calibrationValue);
-	sensors.two.current(5, calibrationValue);
-	sensors.three.current(4, calibrationValue);
+  sensors.one.current(3, calibrationValue);
+  sensors.two.current(5, calibrationValue);
+  sensors.three.current(4, calibrationValue);
 }
 
 static int readSensor(int sensor, int voltage = 230) {
-  return random(10,20);
-	switch (sensor) {
-		case 1:
-			return sensors.one.calcIrms(1480) * voltage;
-			break;
-		case 2:
-			return sensors.two.calcIrms(1480) * voltage;
-			break;
-		case 3:
-			return sensors.three.calcIrms(1480) * voltage;
-			break;
-	}
+  return random(10, 20);
+  switch (sensor) {
+    case 1:
+      return sensors.one.calcIrms(1480) * voltage;
+      break;
+    case 2:
+      return sensors.two.calcIrms(1480) * voltage;
+      break;
+    case 3:
+      return sensors.three.calcIrms(1480) * voltage;
+      break;
+  }
 }
 
 void loop() {
-	Ethernet.maintain();
+  Ethernet.maintain();
 
-	EthernetClient client = server.available();
-	if (client == true) {
+  EthernetClient client = server.available();
+  if (client == true) {
     char reply[100];
-    sprintf(reply, "{\"uptime\": %i, \"rollover\": %i, \"sensors\": {\"1\": %i, \"2\": %i, \"3\": %i}}\n", millis()/1000, readSensor(1), readSensor(2), readSensor(3));
+    sprintf(reply, "{\"uptime\": %i, \"rollover\": %i, \"sensors\": {\"1\": %i, \"2\": %i, \"3\": %i}}\n", millis() / 1000, readSensor(1), readSensor(2), readSensor(3));
     server.print(reply);
-		client.stop();
-	}
+    client.stop();
+  }
 }
